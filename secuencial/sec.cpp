3bb6ff2266f8f6feba1423fa5d1b28e09d50b444
fschonffeldt <f.schonffeldtc@gmail.com>
2023-12-03 01:06:18 -0300

    secuencial v1

--- secuencial/sec.cpp
+++ secuencial/sec.cpp
@@ -5,36 +5,41 @@
 using namespace cv;
 using namespace std;
 
-int main() {
-    // Solicitar al usuario la ruta de la imagen de entrada
-    string inputName;
-    getline(cin,inputName);
-    Mat image = imread(inputName, IMREAD_COLOR);
+void convertirGris(Mat& image) {
+    for (int r = 0; r < image.rows; ++r) {
+        for (int c = 0; c < image.cols; ++c) {
+            Vec3b pixel = image.at<Vec3b>(r, c);
+            uchar gray = 0.299 * pixel[2] + 0.587 * pixel[1] + 0.114 * pixel[0];
+            image.at<Vec3b>(r, c) = Vec3b(gray, gray, gray);
+        }
+    }
+}
 
-    if (image.empty()) {
-        cerr << "Error al cargar la imagen." << endl;
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        cerr << "Uso: " << argv[0] << " <imagenEntrada> <nombreSalida>" << endl;
         return -1;
     }
 
-    // Solicitar al usuario el nombre de la imagen de salida
-    cout << "Ingrese el nombre de la imagen en escala de grises de salida (con extensión, por ejemplo, resultado.jpg): ";
-    string outputName;
-    getline(cin, outputName);
+    string inputImageName = argv[1];
+    string outputImageName = argv[2];
 
-    // Medir el tiempo de ejecución
     auto start_time = chrono::high_resolution_clock::now();
 
-    Mat grayImage;
-    cvtColor(image, grayImage, COLOR_BGR2GRAY);
+    Mat image = imread(inputImageName, IMREAD_COLOR);
 
-    // Medir el tiempo total
+    if (image.empty()) {
+        cerr << "Error al cargar la imagen de entrada." << endl;
+        return -1;
+    }
+
+    convertirGris(image);
+
     auto end_time = chrono::high_resolution_clock::now();
     auto duration = chrono::duration_cast<chrono::milliseconds>(end_time - start_time);
 
-    // Guardar la imagen en escala de grises
-    imwrite(outputName, grayImage, {IMWRITE_JPEG_QUALITY, 95});
+    imwrite(outputImageName, image);
 
-    cout << "Imagen en escala de grises guardada como: " << outputName << endl;
     cout << "Tiempo total de ejecución: " << duration.count() << " ms" << endl;
 
     return 0;
